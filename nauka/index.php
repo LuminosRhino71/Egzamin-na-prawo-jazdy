9a9e3e00e6d48912400250b92db72ec4acd58cb2
LuminosRhino71 <122312870+LuminosRhino71@users.noreply.github.com>
2026-06-18 17:44:35 +0200

    Dodanie podstrony z trybem nauki

---
+++ nauka/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<?php
+    session_start();
+    session_unset();
+?>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Tryb Nauki</title>
+        <link rel="stylesheet" href="../styles.css">
+        <link rel="stylesheet" href="../variable_styles.css">
+    </head>
+    <body>
+        <header>
+            <h1>Ucz się</h1>
+        </header>
+        <main>
+            <a href="../" class="defaultButton">Porzuć naukę</a>
+        </main>
+    </body>
+</html>
