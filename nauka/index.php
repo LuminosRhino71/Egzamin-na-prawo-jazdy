660ca5c634a818e3541f2c6e9d3aa01bfa403be0
LuminosRhino71 <122312870+LuminosRhino71@users.noreply.github.com>
2026-06-25 16:34:01 +0200

    Dodano możliwość zaznaczania odpowiedzi i przechodzenia do następnego pytania.

--- nauka/index.php
+++ nauka/index.php
@@ -59,6 +59,51 @@
                         </div>
                         <p>{$question["Pytanie"]}</p>
                     HTML;
+
+                    echo /*html*/'<form id="answerForm" action="./" method="post">';
+
+                    $radioInputState = "";
+                    $labelSummaryModeClass = "";
+                    $labelAnswerIndicatorsClasses = [
+                        "A" => "",
+                        "B" => "",
+                        "C" => "",
+                        "T" => "",
+                        "N" => ""
+                    ];
+
+                    $summaryMode = isset($_POST["summaryMode"]) && $_POST["summaryMode"] === true;
+                    if ($summaryMode) {
+                        $radioInputState = " disabled";
+                        $labelSummaryModeClass = " deactivateLabel";
+                        $labelAnswerIndicatorsClasses[$test->questions[$currentQuestionNumber]["Poprawna_odp"]] = " correct";
+                        if (!$test->answersCorrectness[$currentQuestionNumber]) {
+                            $labelAnswerIndicatorsClasses[$test->answersChosen[$currentQuestionNumber]] = " incorrectlyChosen";
+                        }
+                    }
+
+                    if ($question["Poprawna_odp"] === "A" || $question["Poprawna_odp"] === "B" || $question["Poprawna_odp"] === "C") {
+                        echo <<<HTML
+                            <input type="radio" id="aAnswer" name="multipleChoiceAnswer" value="A" class="invisibleRadio"{$radioInputState}>
+                            <label for="aAnswer" class='answerRadioLabel{$labelAnswerIndicatorsClasses["A"]}{$labelSummaryModeClass}'>A. {$question["Odp_A"]}</label>
+                            <input type="radio" id="bAnswer" name="multipleChoiceAnswer" value="B" class="invisibleRadio"{$radioInputState}>
+                            <label for="bAnswer" class='answerRadioLabel{$labelAnswerIndicatorsClasses["B"]}{$labelSummaryModeClass}'>B. {$question["Odp_B"]}</label>
+                            <input type="radio" id="cAnswer" name="multipleChoiceAnswer" value="C" class="invisibleRadio"{$radioInputState}>
+                            <label for="cAnswer" class='answerRadioLabel{$labelAnswerIndicatorsClasses["C"]}{$labelSummaryModeClass}'>C. {$question["Odp_C"]}</label>
+                            <input type="radio" id="noAnswer" name="multipleChoiceAnswer" value="NOT ANSWERED" class="invisibleRadio" checked>
+                        HTML;
+                    } else if ($question["Poprawna_odp"] === "T" || $question["Poprawna_odp"] === "N") {
+                        echo <<<HTML
+                            <input type="radio" id="trueAnswer" name="multipleChoiceAnswer" value="T" class="invisibleRadio"{$radioInputState}>
+                            <label for="trueAnswer" class='answerRadioLabel{$labelAnswerIndicatorsClasses["T"]}{$labelSummaryModeClass}'>Tak</label>
+                            <input type="radio" id="falseAnswer" name="multipleChoiceAnswer" value="N" class="invisibleRadio"{$radioInputState}>
+                            <label for="falseAnswer" class='answerRadioLabel{$labelAnswerIndicatorsClasses["N"]}{$labelSummaryModeClass}'>Nie</label>
+                            <input type="radio" id="noAnswer" name="multipleChoiceAnswer" value="NOT ANSWERED" class="invisibleRadio" checked>
+                        HTML;
+                    }
+
+                    echo /*html*/'</form>';
+                    echo /*html*/'<button type="submit" id="submitAnswerButton" class="defaultButton" form="answerForm">Następne pytanie</button>';
                 } else {
                     echo /*html*/'<p class="errorMessage">Wystąpił błąd podczas pobierania pytania.</p>';
                 }
